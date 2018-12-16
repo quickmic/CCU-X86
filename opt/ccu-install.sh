c439e892ddc4351e4c85b799936d69e8eb85f0bb
root <root@test-ccu.rms-systems.intranet>
2018-12-16 11:55:06 +0100

    update installscript

--- opt/ccu-install.sh
+++ opt/ccu-install.sh
@@ -71,3 +71,30 @@ rm /var/www* -R
 
 git clone https://github.com/quickmic/occu.git /opt/occu/
 
+cp /opt/occu/HMserver/opt/HMServer/HMIPServer.jar /opt/HMServer/
+cp /opt/occu/HMserver/opt/HMServer/HMServer.jar /opt/HMServer/
+cp /opt/occu/WebUI/bin/* -R /bin/
+cp /opt/occu/WebUI/www/* -R /www/
+cp /opt/occu/firmware/* -R /firmware/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/HS485D/bin/* -R /bin/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/HS485D/lib/* -R /lib/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/LinuxBasis/* -R /
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/bin/* -R /bin/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/lib/* -R /lib/
+cp /opt/occu/HMserver/opt/HmIP/* -R /opt/HmIP/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/config_templates/rfd.conf -R /etc/config/rfd.conf
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/config_templates/* -R /etc/config_templates/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/crRFD.conf /etc/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/bin/* -R /bin/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/lib/* -R /lib/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/config_templates/* -R /etc/config/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/config_templates/* -R /etc/config_templates/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/rega.conf /etc/config/
+cp /opt/occu/HMserver/etc/config_templates/log4j.xml /etc/config/
+cp /opt/occu/HMserver/opt/HMServer/* -R /opt/HMServer/
+cp /opt/occu/X86_32_Debian_Wheezy/packages/lighttpd/etc/lighttpd/* -R /etc/lighttpd/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI-Beta/bin/* -R /bin/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI-Beta/lib/* -R /lib/
+cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/crRFD.conf /etc/config/crRFD.conf
+
+
