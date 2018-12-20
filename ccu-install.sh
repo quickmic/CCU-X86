0a16350f5cfcc4f6b3535d18a32e815800e4cb1d
root <root@test-ccu.rms-systems.intranet>
2018-12-20 11:16:43 +0100

    update

--- ccu-install.sh
+++ ccu-install.sh
@@ -63,8 +63,27 @@ cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI-Beta/lib/* -R /lib/
 cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/crRFD.conf /etc/config/crRFD.conf
 
 version=`/usr/bin/git -C /opt/occu/ describe --tags`
-echo "VERSION="$version > /boot/VERSION
 /bin/sed -i -n '/WEBUI_VERSION = "/{:a;N;/;/!ba;N;s/.*\n/    WEBUI_VERSION = "'$version'";\n\n/};p' /www/rega/pages/index.htm
+
+#Remove buildID (char) from Version
+chrlen=${#version}
+counter=1
+
+while [ $counter -le $chrlen ]
+do
+        temp=${version:$counter:1}
+
+        if [[ "$temp"  =~ [a-zA-Z] ]]
+        then
+                chrlen=$counter-1
+                break
+        fi
+
+        ((counter++))
+done
+
+version=${version:0:$chrlen}
+echo "VERSION="$version > /boot/VERSION
 
 echo "Enable HMIP? (y/n):"
 read HMIP
