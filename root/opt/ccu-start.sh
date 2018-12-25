bef1946781aab32bf732731366d777f62322400f
root <root@test-ccu.rms-systems.intranet>
2018-12-25 17:22:56 +0100

    addons-patch

--- root/opt/ccu-start.sh
+++ root/opt/ccu-start.sh
@@ -8,18 +8,7 @@ ln -s $(which java) /opt/java/bin/
 /opt/ccu-updateCheck.sh
 
 #Check for Addon Installation
-if [[ -r /usr/local/.doAddonInstall ]]
-then
-        mkdir /tmp/addon
-        cd /tmp/addon
-        tar -C /tmp/addon --no-same-owner --no-same-permissions -xf /usr/local/tmp/new_addon.tar.gz
-        /bin/sed -i 's/usr\/local\/etc/\etc/g' /tmp/addon/update_script
-        /bin/sed -i 's/usr\/local\/addons/etc\/config\/addons/g' /tmp/addon/update_script
-	/bin/sed -i 's/\.\/update_addon/\/bin\/update_addon/g' /tmp/addon/update_script
-	/bin/sed -i 's/${ADDON_DIR}\/update_addon/\/bin\/update_addon/g' /tmp/addon/update_script
-        /tmp/addon/update_script HM-RASPBERRYMATIC
-        rm -f /usr/local/.doAddonInstall
-fi
+/opt/ccu-addonInstall.sh
 
 #Start Addons
 /bin/run-parts -a start /etc/config/rc.d
