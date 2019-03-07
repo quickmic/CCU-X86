020a535033a5db351f250fc59238fdd7526e3748
root <root@ccu-test2.rms-systems.intranet>
2019-03-07 15:12:56 +0100

    update

--- root/opt/ccu-config.sh
+++ root/opt/ccu-config.sh
@@ -4,7 +4,36 @@ rm -f /var/status/HMIPlocaldevice
 rm -f /var/status/HMIPremserialhost
 rm -f /var/status/HMIPenabled
 
-cp -f /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/config_templates/InterfacesList.xml /etc/config/InterfacesList.xml
+cp -f /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/config_templates/InterfacesList.xml /etc/config/
+cp -f /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/config_templates/rfd.conf /etc/config/
+cp -f /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/config_templates/multimacd.conf /etc/config/
+
+patch --forward -d / -p0 < /opt/occu-x86/patches/0012-multimacd.patch
+patch --forward -d / -p0 < /opt/occu-x86/patches/0003-rfd-interface.patch
+
+#Check if running in lxc container
+if ! grep lxc /proc/1/environ -qa
+then
+        while true
+        do
+                read -r -p "Are you using a HB-RF-USB interface? (y/n): " HBRFUSB
+
+                if [ "$HBRFUSB" = "y" ]
+                then
+                        cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/config_templates/multimacd.conf /etc/config/
+                        apt-get install build-essential -y
+                        apt-get -t stretch-backports install linux-image-amd64 -y
+                        apt-get -t stretch-backports install linux-headers-amd64 -y
+                        /opt/occu-x86/kernel-modules/compile.sh
+                        break
+                elif [ "$HBRFUSB" = "n" ]
+                then
+                        rm /etc/config/multimacd.conf
+                        rm /etc/init.d/S60multimacd
+                        break
+                fi
+        done
+fi
 
 while true
 do
@@ -12,7 +41,15 @@ do
 
         if [ "$HMIP" = "y" ]
         then
-                echo "ttyUSB0" > /var/status/HMIPenabled
+                if [ "$HBRFUSB" = "y" ]
+                then
+                        echo "mmd_hmip" > /var/status/HMIPenabled
+                        /bin/sed -i 's/Adapter.1.Port=\/dev\/ttyS0/Adapter.1.Port=\/dev\/mmd_hmip/g' /etc/config/crRFD.conf
+                else
+                        echo "ttyUSB0" > /var/status/HMIPenabled
+                        /bin/sed -i 's/Adapter.1.Port=\/dev\/ttyUSB0/Adapter.1.Port=\/dev\/ttyUSB0/g' /etc/config/crRFD.conf
+                fi
+
                 break
         elif [ "$HMIP" = "n" ]
         then
@@ -27,7 +64,18 @@ do
 
         if [ "$BIDCOS" = "y" ]
         then
-                touch /var/status/BIDCOSenable
+                if [ "$HBRFUSB" = "y" ]
+                then
+                        echo "mmd_bidcos" > /var/status/BIDCOSenable
+                        echo "[Interface 0]" >> /etc/config/rfd.conf
+                        echo "Type = CCU2" >> /etc/config/rfd.conf
+                        echo "ComPortFile = /dev/mmd_bidcos" >> /etc/config/rfd.conf
+                        echo "AccessFile = /dev/null" >> /etc/config/rfd.conf
+                        echo "ResetFile = /dev/null" >> /etc/config/rfd.conf
+                else
+                        echo "gateways" > /var/status/BIDCOSenable
+                fi
+
                 break
         elif [ "$BIDCOS" = "n" ]
         then
--- root/opt/ccu-update.sh
+++ root/opt/ccu-update.sh
@@ -17,9 +17,9 @@ fi
 rm /www/* -R
 
 #Debian update
-apt-get update
-apt-get dist-upgrade -y
-apt-get clean
+#apt-get update
+#apt-get dist-upgrade -y
+#apt-get clean
 
 #Kill processes
 killall socat
@@ -38,7 +38,6 @@ cp -rf /opt/occu-x86/root/opt/* /opt/
 cp -rf /opt/occu-x86/root/www/* /www/
 cp -rf /opt/occu-x86/root/etc/init.d/* /etc/init.d/
 
-#git clone https://github.com/quickmic/occu.git /opt/occu/
 cp /opt/occu/HMserver/opt/HMServer/HMIPServer.jar /opt/HMServer/
 cp /opt/occu/HMserver/opt/HMServer/HMServer.jar /opt/HMServer/
 cp -rf /opt/occu/WebUI/bin/* /bin/
@@ -50,18 +49,11 @@ cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/LinuxBasis/* /
 cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/bin/* /bin/
 cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/lib/* /lib/
 cp -rf /opt/occu/HMserver/opt/HmIP/* /opt/HmIP/
-#cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/config_templates/rfd.conf /etc/config/rfd.conf
-#cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/config_templates/* /etc/config_templates/
 cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/bin/* /bin/
 cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/lib/* /lib/
-#cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/* /etc/
-#cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI/etc/config_templates/* /etc/config/
-#cp /opt/occu/HMserver/etc/config_templates/log4j.xml /etc/config/
 cp -rf /opt/occu/HMserver/opt/HMServer/* /opt/HMServer/
-#cp -rf /opt/occu/X86_32_Debian_Wheezy/packages/lighttpd/etc/lighttpd/* /etc/lighttpd/
 cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI-Beta/bin/* /bin/
 cp -rf /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/WebUI-Beta/lib/* /lib/
-#cp /opt/occu/X86_32_Debian_Wheezy/packages-eQ-3/RFD/etc/crRFD.conf /etc/config/crRFD.conf
 
 versionOCCU=`git -C /opt/occu/ describe --tags`
 versionX86=`git -C /opt/occu-x86/ describe --tags`
@@ -90,22 +82,11 @@ echo "VERSION="$version > /boot/VERSION
 echo "CCU Update Log:" > /var/log/ccuupdate.txt
 
 #Apply patches
-for f in /opt/occu-x86/patches/0*
+for f in /opt/occu-x86/patches/*
 do
 	echo $f >> /var/log/ccuupdate.txt
         patch --forward -d / -p0 < $f >> /var/log/ccuupdate.txt
 done
-
-
-if [ -f /var/status/ExtendedFeatures ]
-then
-
-	for f in /opt/occu-x86/patches/1*
-	do
-		echo $f >> /var/log/ccuupdate.txt
-		patch --forward -d / -p0 < $f >> /var/log/ccuupdate.txt
-	done
-fi
 
 find /etc/ -type f -name '*.rej' -delete
 find /etc/ -type f -name '*.orig' -delete
