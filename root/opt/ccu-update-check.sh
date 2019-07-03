aab90ea5f228ca82d4174263cb1d61ceb41d9351
root <root@homecontrol.rms-systems.intranet>
2019-07-03 16:15:36 +0200

    update info fixes

--- root/opt/ccu-update-check.sh
+++ root/opt/ccu-update-check.sh
@@ -5,6 +5,6 @@ git -C /opt/occu-x86/ pull
 versionOCCU=`git -C /opt/occu/ describe --tags`
 versionX86=`git -C /opt/occu-x86/ describe --tags`
 versionCombined=$versionOCCU" / "$versionX86
-echo "homematic.com.setLatestVersion('"$versionCombined"', 'HM-CCU3');" > /www/ccuupdate.txt
+echo "homematic.com.setLatestVersion('"$versionCombined"', 'HM-CCU3');" > /www/ccuupdate.js
 cp /opt/occu-x86/root/opt/ccu-update.sh /opt/ccu-update.sh
 
